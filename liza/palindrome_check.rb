9db058490a1ebff57e733b49f37bd57b2dbb9d8f
lizaaadrozd <elizavetadrozd266@gmail.com>
2024-09-19 22:40:15 +0300

    added two files

---
+++ liza/palindrome_check.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+def palindrome_check(input_string)
+  result = if input_string == input_string.reverse
+             'String is palindrome!'
+           else
+             'String is not palindrome!'
+           end
+  puts result
+end
+
+puts 'Hello! Enter a string to check for a palindrome: '
+palindrome_check(gets.chomp)
